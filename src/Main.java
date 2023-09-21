fa1c7bcd494a0778120d6f1e76e2e8c809612763
robertbodrug <robertobodrug@gmail.com>
2023-09-21 23:18:26 +0200

    PhoneNumber final

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
-
         System.out.println(PhoneNumberSearcher.searchNumber(new File("src\\file.txt")));
         System.out.println();
     }
